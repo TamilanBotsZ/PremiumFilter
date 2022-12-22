790fa07a8bdfa10fd9f16edb5a736b5e18971146
VoiceOfSha <116538465+VoiceOfSha@users.noreply.github.com>
2022-12-22 17:32:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,3 +63,9 @@ LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_I
 LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
 LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
+
+
+SHORTNER_API = environ.get("SHORTNER_API", "d7e6bf4c5666744fbcd6aa8ba0a08d4cac41e27a")
+LONG_DROPLINK_URL = environ.get("LONG_DROPLINK_URL", False)
+
+ 
