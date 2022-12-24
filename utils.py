6e6987eaa0a787d8ca826b0637be50ce3f2cb6af
VoiceOfSha <116538465+VoiceOfSha@users.noreply.github.com>
2022-12-24 08:30:25 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -385,6 +385,6 @@ async def get_shortlink(link):
         if LONG_DROPLINK_URL =="True" or LONG_DROPLINK_URL is True:
             return await shortz.get_quick_link(link)
         else:
-            return await shortz.convert(link, silently.fail=false)
+            return await shortz.convert(link, silently_fail=False)
 
     return link
