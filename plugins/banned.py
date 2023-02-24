e9859d412985ad0f0d066ce38201468e83c67018
VoiceOfSha <116538465+VoiceOfSha@users.noreply.github.com>
2023-02-24 17:47:20 +0530

    Update banned.py

--- plugins/banned.py
+++ plugins/banned.py
@@ -21,17 +21,17 @@ disabled_group=filters.create(disabled_chat)
 @Client.on_message(filters.private & banned_user & filters.incoming)
 async def ban_reply(bot, message):
     ban = await db.get_ban_status(message.from_user.id)
-    await message.reply(f'Sorry Dude, You are Banned to use Me. \nBan Reason: {ban["ban_reason"]}')
+    await message.reply(f'sᴏʀʀʏ ᴜ ʀᴇ ʙᴀɴɴᴇᴅ ʙʏ ᴀᴅᴍɪɴ. \nʙᴀɴ ʀᴇᴀsᴏɴ: {ban["ban_reason"]}')
 
 @Client.on_message(filters.group & disabled_group & filters.incoming)
 async def grp_bd(bot, message):
     buttons = [[
-        InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
+        InlineKeyboardButton('sᴜᴘᴘᴏʀᴛ', url=f'https://t.me/{SUPPORT_CHAT}')
     ]]
     reply_markup=InlineKeyboardMarkup(buttons)
     vazha = await db.get_chat(message.chat.id)
     k = await message.reply(
-        text=f"CHAT NOT ALLOWED 🐞\n\nMy admins has restricted me from working here ! If you want to know more about it contact support..\nReason : <code>{vazha['reason']}</code>.",
+        text=f"ᴄʜᴀᴛ ɴᴏᴛ ᴀʟʟᴏᴡᴇᴅ 🐞\n\nᴜ ʜᴀᴠᴇ ʙᴀɴɴᴇᴅ ᴅᴜᴇ ᴛᴏ ɪʟʟᴇɢᴀʟ ᴜsᴀɢᴇ ᴏғ ᴍᴇ .. ɪғ ᴜ ᴜsᴇ ᴍᴇ ɴᴏᴡ ᴀғᴛᴇʀ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛᴀᴄᴛ sᴜᴘᴘᴏʀᴛ ᴏʀ ᴏᴡɴᴇʀ \nReason : <code>{vazha['reason']}</code>.",
         reply_markup=reply_markup)
     try:
         await k.pin()
