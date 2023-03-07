d82f565bc11870b3fd21ec1339edbbfb778cc32b
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-07 22:09:10 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,7 +17,6 @@ from typing import List
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
 import requests
-import aiohttp
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
