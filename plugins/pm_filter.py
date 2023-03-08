0829262a0194ff75a5348aa67d9a1f72c1d85925
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-08 08:16:17 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -487,7 +487,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]       
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.FEATURES_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.FEATUREE_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
          )
