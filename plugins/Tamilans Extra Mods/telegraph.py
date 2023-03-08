55e356e58ec2132538cddd7738b90db48c9d692f
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-08 19:09:07 +0530

    Update telegraph.py

--- plugins/Tamilans Extra Mods/telegraph.py
+++ plugins/Tamilans Extra Mods/telegraph.py
@@ -10,20 +10,20 @@ from utils import get_file_id
 async def telegraph_upload(bot, update):
     replied = update.reply_to_message
     if not replied:
-        await update.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
+        await update.reply_text("Rᴇᴩʟʏ Tᴏ A Pʜᴏᴛᴏ / Vɪᴅᴇᴏ Uɴᴅᴇʀ 5ᴍʙ")
         return
     file_info = get_file_id(replied)
     if not file_info:
-        await update.reply_text("Not supported!")
+        await update.reply_text("Nᴏᴛ Sᴜᴩᴩᴏʀᴛᴇᴅ Mᴇᴅɪᴀ Tʏᴩᴇ !")
         return
-    text = await update.reply_text(text="<code>Downloading to My Server ...</code>", disable_web_page_preview=True)   
+    text = await update.reply_text(text="<code>Dᴏᴡɴʟᴏᴀᴅɪɴɢ Iɴ Mʏ Sᴇʀᴠᴇʀ...</code>", disable_web_page_preview=True)   
     media = await update.reply_to_message.download()   
-    await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)                                            
+    await text.edit_text(text="<code>Dᴏᴡɴʟᴏᴀᴅ Tᴏ Mʏ Sᴇʀᴠᴇʀ Is Cᴏᴍᴩʟᴇᴛᴇ. Nᴏᴡ IᴀM Uᴩʟᴏᴀᴅɪɴɢ Tᴏ Tᴇʟᴇɢʀᴀᴩʜ ...</code>", disable_web_page_preview=True)                                            
     try:
         response = upload_file(media)
     except Exception as error:
         print(error)
-        await text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)       
+        await text.edit_text(text=f"Eʀʀᴏʀ :- {error}", disable_web_page_preview=True)       
         return    
     try:
         os.remove(media)
@@ -31,13 +31,13 @@ async def telegraph_upload(bot, update):
         print(error)
         return    
     await text.edit_text(
-        text=f"<b>Link :-</b>\n\n<code>https://graph.org{response[0]}</code>",
+        text=f"<b>Lɪɴᴋ :-</b>\n\n<code>https://graph.org{response[0]}</code>",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup( [[
             InlineKeyboardButton(text="Open Link", url=f"https://graph.org{response[0]}"),
             InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
             ],[
-            InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
+            InlineKeyboardButton(text="✗ Cʟᴏsᴇ ✗", callback_data="close")
             ]])
         )
     
