c679c39bb4de38d28b20316e3233440a9569af46
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-09 19:15:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TamilanMoviesChat')
 
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 
-IMDB = is_enabled((environ.get('IMDB', "False")), False)
+IMDB = is_enabled((environ.get('IMDB', "True")), True)
 
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
 
