cfee532633e91815db00ffac3f6a4bbc99f2c9a8
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-10 06:54:29 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from aiohttp import web
+from plugins import web_server
 
 PORT = "8080"
 
@@ -42,10 +44,10 @@ class Bot(Client):
         temp.U_NAME = me.username
         temp.B_NAME = me.first_name
         self.username = '@' + me.username
-
-
-  # Koyeb Support Removed # Due To Error
-
+        app = web.AppRunner(await web_server())
+        await app.setup()
+        bind_address = "0.0.0.0"
+        await web.TCPSite(app, bind_address, PORT).start()
         logging.info(f"{me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}.")
         logging.info(LOG_STR)
 
