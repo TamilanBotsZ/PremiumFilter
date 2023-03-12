554a6319e6e42140a59bb739cbd2c5e767978cbb
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-12 19:03:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,18 +1,19 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTNER_SITE, SHORTNER_API
-from imdb import Cinemagoer 
+from info import *
+from imdb import Cinemagoer
+from datetime import datetime, date
+import string
+from Script import script
+import pytz
+import random
 import asyncio
 from pyrogram.types import Message, InlineKeyboardButton
 from pyrogram import enums
 from typing import Union
-from Script import script
-import pytz
-import random 
 import re
 import os
-from datetime import datetime, date
-import string
+from datetime import datetime
 from typing import List
 from database.users_chats_db import db
 from bs4 import BeautifulSoup
