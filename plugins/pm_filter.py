4d7ff78bd2ae44e19e638fd0b3716501232894df
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-13 19:13:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -819,7 +819,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"📂Tᴏᴛᴀʟ Fɪʟᴇs Fᴏᴜɴᴅᴇᴅ : {str(total_results)} /n😙Rᴇǫᴜᴇsᴛᴇᴅ Bʏ {message.from_user.mention} /n😊 Yᴏᴜʀ Qᴜᴇʀʏ {search}"
+        cap = f"😙Rᴇǫᴜᴇsᴛᴇᴅ Bʏ {message.from_user.mention} /n😊 Yᴏᴜʀ Qᴜᴇʀʏ {search} /n📂Tᴏᴛᴀʟ Fɪʟᴇs Fᴏᴜɴᴅᴇᴅ : {str(total_results)}"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
