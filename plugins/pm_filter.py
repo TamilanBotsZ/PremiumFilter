6f3eda080d7d214f900bd149d13e009f43b6f4a3
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-16 18:10:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -430,6 +430,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             chat_id=query.from_user.id,
             file_id=file_id,
             caption=f_caption,
+            ( [ [ InlineKeyboardButton('🚸 ᴅᴇʟᴇᴛᴇ', callback_data='close_data') ] ] ),
             protect_content=True if ident == 'checksubp' else False
         )
     elif query.data == "pages":
