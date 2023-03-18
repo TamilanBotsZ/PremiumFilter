a55e7b76f4ee14b6d9459add122e573b15d72dc3
Sharath <121667705+TamilanBotsZ@users.noreply.github.com>
2023-03-18 17:52:19 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -448,28 +448,3 @@ def humanbytes(size):
         size /= power
         n += 1
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B' 
-
-
-async def get_shortlink(link):
-    https = link.split(":")[0]
-    if "http" == https:
-        https = "https"
-        link = link.replace("http", https)
-    url = f'https://{SHORTNER_SITE}/api'
-    params = {'api': SHORTNER_API,
-              'url': link,
-              }
-
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                data = await response.json()
-                if data["status"] == "success":
-                    return data['shortenedUrl']
-                else:
-                    logger.error(f"Error: {data['message']}")
-                    return f'https://{SHORTNER_SITE}/api?api={SHORTNER_API}&link={link}'
-
-    except Exception as e:
-        logger.error(e)
-        return f'{SHORTNER_SITE}/api?api={SHORTNER_API}&link={link}'
