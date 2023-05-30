f93d4fec4e9039d35179b7c1690c709e97dbdf81
1238muj <122881477+1238muj@users.noreply.github.com>
2023-05-30 13:37:25 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Hᴇʟᴘ ℹ️', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 ᴜᴘᴅᴀᴛᴇs 📢', url='https://t.me/TamilanBotsZ')
+            InlineKeyboardButton('📢 ᴜᴘᴅᴀᴛᴇs 📢', url='https://t.me/+HMo4S0heOUllMWVl')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
